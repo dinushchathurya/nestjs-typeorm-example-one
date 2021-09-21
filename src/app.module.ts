6d79c22d4cb57fc9ba7db6ae70fb3b653540bce6
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-21 20:36:57 +0530

    hammer: Remove unused app specs, service and controller file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
@@ -9,7 +7,7 @@ import { UsersModule } from './modules/users/users.module';
     TypeOrmModule.forRoot(),
     UsersModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
