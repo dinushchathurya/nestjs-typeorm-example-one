c006d3d142c771cbc959f102500c472e3b59cb36
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-21 21:20:26 +0530

    :bookmark: Finish basic TypeOrm CRUD with basic validation

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,7 +5,9 @@ import { UsersService } from './users.service';
 import { User } from 'src/entities/user.entity';
 
 @Module({
-  imports:  [TypeOrmModule.forFeature([User])],
+  imports:  [TypeOrmModule.forFeature(
+    [User]
+  )],
   controllers: [UsersController],
   providers: [UsersService]
 })
